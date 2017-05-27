8dd931c6c5d6d104a7030b9e20d83ab451628982
Omnomon <jessselu@gmail.com>
2017-05-27 01:03:45 -0700

    chat works

--- assets/javascript.js
+++ assets/javascript.js
@@ -22,22 +22,22 @@ $(document).ready(function() {
     var thisPlayerNumber = 0;
     var playerId_;
     var postKey;
-    var player1Choice;
-    var player2Choice;
+    var playerChoice;
     var postKey = [];
     postKey = database.ref().push().key
     console.log(postKey)
-
+    var wins = 0;
+    var losses = 0;
     var picks = [];
     var keys = [];
     var childData;
     var childKey;
     var playerIs
-    //---------------------------------------------------------------------------
-    //
-    // everytime a new connection happens, add it to firebase. Limit to 2 connections
-    //
-    //---------------------------------------------------------------------------
+        //---------------------------------------------------------------------------
+        //
+        // everytime a new connection happens, add it to firebase. Limit to 2 connections
+        //
+        //---------------------------------------------------------------------------
 
 
     data = {
@@ -52,7 +52,7 @@ $(document).ready(function() {
                 database.ref("/users/" + postKey).set(data)
                 database.ref("/users/" + postKey).onDisconnect().remove()
                 var playerNumber = snap.numChildren()
-                playerIs = "Player " +(playerNumber + 1)
+                playerIs = "Player " + (playerNumber + 1)
                 $(".playerNumber").html(playerIs)
             }
         })
@@ -77,8 +77,8 @@ $(document).ready(function() {
     choices.forEach(function(choice, i) {
         map[choice] = {};
         map[choice][choice] = "Was a tie"
-        map[choice][choices[(i + 1) % 3]] = choices[(i + 1) % 3] + " wins!"
-        map[choice][choices[(i + 2) % 3]] = choice + " wins!"
+        map[choice][choices[(i + 1) % 3]] = choices[(i + 1) % 3] + " wins"
+        map[choice][choices[(i + 2) % 3]] = choice + " wins"
     })
 
     function compare(choice1, choice2) {
@@ -120,27 +120,35 @@ $(document).ready(function() {
 
 
     $(document).on("click", "#rock", function(event) {
-        console.log("rock works")
         event.preventDefault();
+        $(".winnerIs").html("")
         database.ref("/users/" + postKey).set({
             choice: "rock"
         })
+        playerChoice = "rock"
+        console.log("rock works")
 
     }).blur()
 
     $(document).on("click", "#paper", function(event) {
         event.preventDefault();
+        $(".winnerIs").html("")
         database.ref("/users/" + postKey).set({
             choice: "paper"
         })
+        playerChoice = "paper"
+
         console.log("paper works")
     }).blur()
 
     $(document).on("click", "#scissors", function(event) {
         event.preventDefault();
+        $(".winnerIs").html("")
         database.ref("/users/" + postKey).set({
             choice: "scissors"
         })
+        playerChoice = "scissors"
+
         console.log("scissors works")
     }).blur()
 
@@ -149,12 +157,31 @@ $(document).ready(function() {
     // Chat functionality
     //
     //---------------------------------------------------------------------------
+    database.ref("/chat").set({})
 
-    $(".chatSend").click(function(){
-      var chatText = $(".chatText").val()
-      database.ref("/chat").set({chatText})
-      $(".chatText").val() = "";
-      
+    $(".chatSend").click(function() {
+        var chatText = $(".chatText").val()
+        database.ref("/chat").push({ chatText })
+        $(".chatText").val("")
     })
 
+    $(document).keyup(function(event) {
+        if (event.keyCode === 13) {
+            var chatText = $(".chatText").val()
+            database.ref("/chat").push({ chatText })
+            $(".chatText").val("")
+        }
+    })
+
+    var startListening = function() {
+        database.ref("/chat").on("child_added", function(snap) {
+            var msg = snap.val()
+            var msgTextElement = $("<p>").html(playerIs + ": " + msg.chatText)
+            var msgElement = $("<div>").addClass("post")
+            msgElement.append(msgTextElement)
+            $(".chatDisplay").append(msgElement)
+        })
+    }
+
+    startListening()
 })
